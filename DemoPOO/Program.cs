e0c86f874b70bb29f42b8afdca8bf81ac9839014
Florencia Saglietti <florsaglietti@hotmail.com>
2022-05-26 22:58:53 -0300

    Creo aplicación para determinar el tipo de transporte y cantidad de pasajeros

---
+++ DemoPOO/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DemoPOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var ColeccionTransporte = new List<TransportePublico>
+            {
+                new Omnibus (100),
+                new Omnibus (70),
+                new Omnibus (50),
+                new Omnibus (20),
+                new Omnibus (10),
+                new Taxi (5),
+                new Taxi (4),
+                new Taxi (3),
+                new Taxi (2),
+                new Taxi (1)
+
+             };
+            foreach (var c in ColeccionTransporte)
+            {
+                Console.WriteLine (c.TipoTransporte + ": cantidad de pasajeros " + c.Pasajeros);
+            }
+        }
+    }
+}

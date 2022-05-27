46b84bba22449d95162bdbee201b2d803ac7f488
Florencia Saglietti <florsaglietti@hotmail.com>
2022-05-26 22:48:21 -0300

    Creo aplicación para determinar el tipo de transporte y cantidad de pasajeros

---
+++ DemoPOO/Omnibus.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoPOO
+{
+    public class Omnibus : TransportePublico
+    {
+        public Omnibus(int pasajeros) : base(pasajeros, "Omnibus" ) { }
+        public override string Avanzar()
+        {
+            return "Avanza";
+        }
+
+        public override string Detenerse()
+        {
+            return "Se detiene";
+        }
+    }
+}
---
+++ DemoPOO/Taxi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoPOO
+{
+    public class Taxi : TransportePublico
+    {
+        public Taxi (int pasajeros) :base(pasajeros, "Taxi") { }
+        public override string Avanzar()
+        {
+            return "Avanza";
+        }
+
+        public override string Detenerse()
+        {
+            return "Se detiene";
+        }
+    }
+}

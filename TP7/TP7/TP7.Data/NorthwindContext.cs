38b153a753ad5290ecf877d0ce01ba2365497a04
Florencia Saglietti <florsaglietti@hotmail.com>
2022-06-19 19:06:13 -0300

    TP7

---
+++ TP7/TP7/TP7.Data/NorthwindContext.cs
@@ -0,0 +1,56 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using TP7.Entities;
+
+namespace TP7.Data
+{
+    public partial class NorthwindContext : DbContext
+    {
+        public NorthwindContext()
+            : base("name=NorthwindConnection")
+        {
+        }
+
+        public virtual DbSet<Categories> Categories { get; set; }
+        public virtual DbSet<Employees> Employees { get; set; }
+        public virtual DbSet<Order_Details> Order_Details { get; set; }
+        public virtual DbSet<Orders> Orders { get; set; }
+        public virtual DbSet<Territories> Territories { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employees>()
+                .HasMany(e => e.Employees1)
+                .WithOptional(e => e.Employees2)
+                .HasForeignKey(e => e.ReportsTo);
+
+            modelBuilder.Entity<Employees>()
+                .HasMany(e => e.Territories)
+                .WithMany(e => e.Employees)
+                .Map(m => m.ToTable("EmployeeTerritories").MapLeftKey("EmployeeID").MapRightKey("TerritoryID"));
+
+            modelBuilder.Entity<Order_Details>()
+                .Property(e => e.UnitPrice)
+                .HasPrecision(19, 4);
+
+            modelBuilder.Entity<Orders>()
+                .Property(e => e.CustomerID)
+                .IsFixedLength();
+
+            modelBuilder.Entity<Orders>()
+                .Property(e => e.Freight)
+                .HasPrecision(19, 4);
+
+            modelBuilder.Entity<Orders>()
+                .HasMany(e => e.Order_Details)
+                .WithRequired(e => e.Orders)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Territories>()
+                .Property(e => e.TerritoryDescription)
+                .IsFixedLength();
+        }
+    }
+}
